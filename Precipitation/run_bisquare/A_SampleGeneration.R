646f006c8126385dddb4479ee5f91ef0aa32d6af
Seiyon Ben Lee <benlee55@gmail.com>
2023-10-05 18:38:55 -0400

    Add files via upload

---
+++ Precipitation/run_bisquare/A_SampleGeneration.R
@@ -0,0 +1,45 @@
+########################################################
+# A_SampleGeneration_Cluster.R 
+# Partitions spatial domain into clusters
+########################################################
+rm(list=ls())
+library(fields);library(classInt)
+load("../samples/water_vapor_20230622.RData")
+set.seed(12345)
+
+# Process Data 
+coords.all<-locs # Coordinates 
+range.coords<-apply(coords.all,2,range) # Rescale data to be on the unit grid
+coords.all<-coords.all+matrix(-(range.coords[1,]),nrow=nrow(coords.all),ncol=2,byrow = TRUE)
+range.coords<-apply(coords.all,2,range);range.coords
+coords.all<-coords.all*matrix(1/abs(range.coords[2,]),nrow=nrow(coords.all),ncol=2,byrow = TRUE)
+apply(coords.all,2,range)
+
+# Split sample into training and testing datasets
+nTot<-length(z)
+nCV=floor(0.1*nTot)
+nMod=nTot-nCV;
+idMod<-sample(1:nTot, nMod)
+idCV<-(1:nTot)[-idMod]
+
+# Generate Split into Training and Testing Samples
+comboLocation<-as.matrix(coords.all)
+rm(coords.all)
+gridLocationMod<-comboLocation[idMod,]
+gridLocationCV<-comboLocation[idCV,]
+XMatFull<-cbind(1,comboLocation[,1])
+XMat<-XMatFull[idMod,]
+XMatCV<-XMatFull[idCV,]
+
+#Observations
+obsFull<-z # Observations
+obsMod<-z[idMod]
+obsCV<-z[idCV]
+
+# Save Data
+save(comboLocation,gridLocationCV, gridLocationMod, 
+     XMatFull, XMat, XMatCV, 
+     idMod, idCV, 
+     obsFull, obsCV, obsMod,
+     file="../samples/precipData.RData")
+
---
+++ Precipitation/run_bisquare_small/E_analysis.R
@@ -0,0 +1,34 @@
+rm(list=ls())
+library(fields);library(classInt); library(nimble)
+####################################
+load("../samples/precipData.RData")
+load("../samples/BisquareBasisFunctions.RData")
+source("source/sharedFunctions.R") # File with useful functions
+n = nrow(gridLocationMod)
+n.pred = nrow(gridLocationCV)
+CVgridLocation<-gridLocationCV
+rm(gridLocationCV,gridLocationMod,comboLocation, XMatFull, obsFull)
+distA_CV<-rdist(CVgridLocation,knotsA) 
+distB_CV<-rdist(CVgridLocation,knotsB)
+distC_CV<-rdist(CVgridLocation,knotsC)
+basisA_CV<-bisquareFunc(dist = distA_CV , eps = epsA) # Basis Functions - Resolution A
+basisB_CV<-bisquareFunc(dist = distB_CV , eps = epsB) # Basis Functions - Resolution B
+basisC_CV<-bisquareFunc(dist = distC_CV , eps = epsC) # Basis Functions - Resolution C
+rm(distA_CV,distB_CV,distC_CV)
+M_CV<-cbind(basisA_CV,basisB_CV,basisC_CV)
+rm(basisA_CV,basisB_CV,basisC_CV)
+
+
+source("source/batchmeans.R")# batchmeans source
+load("../output/mcmcPrecip_small.RData")
+betaMat<-chain_output[,1:2]
+deltaMat<-chain_output[,3:86]
+linearPred<-XMatCV%*%apply(betaMat,2,mean) + M_CV%*%apply(deltaMat,2,mean)
+
+predCV<-expectedValue<-exp(linearPred)
+
+# Prediction - Root Mean squared error
+rmspe<-sqrt(mean((predCV-obsCV)^2))
+
+# Summary: Seems like the global option performs better 
+save(rmspe,predCV,file="../output/resultsMCMC_small.RData")
